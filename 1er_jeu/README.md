2feed6528761f9f395a45dea652b411cf544bfd9
falanpala <falanpala@gmail.com.com>
2026-04-14 21:05:21 +0200

    corection bug3

---
+++ 1er_jeu/README.md
@@ -0,0 +1,13 @@
+Mon jeu Python
+
+Projet réalisé en suivant : https://www.youtube.com/watch?v=ooITOxbYVTo&t=1078s
+ (tutoriel de Graven)
+
+⚠️ Les images ne sont pas incluses sur mon github.
+
+Pour lancer le jeu :
+Télécharger les assets depuis la vidéo
+Les placer dans un dossier nommé assets
+Lancer main.py
+
+Je vous conseille d’aller voir son tutoriel, il est très bien fait et accessible à tous.
--- 1er_jeu/game.py
+++ 1er_jeu/game.py
@@ -1,6 +1,7 @@
 import pygame
 import pytmx
 import pyscroll
+from player import Player          # ← import ajouté
 
 class Game:
     def __init__(self):
@@ -10,17 +11,18 @@ class Game:
         tmx_data = pytmx.util_pygame.load_pygame('1er_jeu/carte.tmx')
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer.zoom = 2
 
+        self.player = Player()
         self.groupe = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        self.groupe.add(self.player)
 
     def run(self):
         running = True
         while running:
             self.groupe.draw(self.screen)
             pygame.display.flip()
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-
         pygame.quit()
